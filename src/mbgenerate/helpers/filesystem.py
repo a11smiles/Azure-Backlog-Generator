a49e87d09044626ffd27c9b534d45e5b5185f9c6
Joshua Davis <me@jdav.is>
2020-05-07 13:30:11 -0400

    adding filesystem mock tests

--- src/mbgenerate/helpers/filesystem.py
+++ src/mbgenerate/helpers/filesystem.py
@@ -1,15 +1,23 @@
 import os
 
 class FileSystem():
+    parentPath = True
 
     def getFiles(self, path):
         files = []
         (root, dirNames, fileNames) = next(os.walk(path))
 
+        if self.parentPath == True and len(fileNames) > 0:
+            raise FileExistsError("parent path should not contain any files")
+        elif self.parentPath == False and 'metadata.json' not in fileNames:
+            raise FileNotFoundError(f"'metadata.json' does not exist in path '{path}'")
+
         fileNames.sort()
         for fileName in fileNames:
             if fileName == 'metadata.json':
                 files.append(os.path.join(path, fileName))
+
+        self.parentPath = False
 
         dirNames.sort()
         for dirName in dirNames:
--- tests/helpers/test_filesystem.py
+++ tests/helpers/test_filesystem.py
@@ -1,8 +1,8 @@
+import pytest
 from pyfakefs import fake_filesystem
 import src.mbgenerate.helpers as helpers
 
 def _mockCorrectFileSystem(fs):
-    fs.create_file('./correct/metadata.json')
     fs.create_file('./correct/01_folder/metadata.json')
     fs.create_file('./correct/01_folder/attachment.doc')
     fs.create_file('./correct/01_folder/01_folder/metadata.json')
@@ -15,9 +15,33 @@ def _mockCorrectFileSystem(fs):
     fs.create_file('./correct/03_folder/metadata.json')
     fs.create_file('./correct/03_folder/03_folder/metadata.json')
 
-def test_getFiles(fs):
+def _mockParentPathHasFileFileSystem(fs):
+    fs.create_file('./parentPathHasFile/metadata.json')
+    fs.create_file('./parentPathHasFile/01_folder/metadata.json')
+    fs.create_file('./parentPathHasFile/01_folder/01_folder/metadata.json')
+
+def _mockPathHasNoMetadataFileSystem(fs):
+    fs.create_file('./pathHasNoMetadata/01_folder/01_folder/metadata.json')
+
+def test_getFiles_CorrectFileSystem(fs):
     _mockCorrectFileSystem(fs)
 
     f = helpers.FileSystem()
     files = f.getFiles('./correct')
-    assert len(files) == 10
+    assert len(files) == 9
+
+def test_getFiles_ParentPathHasFileFileSystem(fs):
+    _mockParentPathHasFileFileSystem(fs)
+
+    f = helpers.FileSystem()
+    with pytest.raises(FileExistsError) as exc:
+        files = f.getFiles('./parentPathHasFile')
+    assert "parent path should not contain any files" in str(exc.value)
+    
+def test_getFiles_PathHasNoMetadata(fs):
+    _mockPathHasNoMetadataFileSystem(fs)
+
+    f = helpers.FileSystem()
+    with pytest.raises(FileNotFoundError) as exc:
+        files = f.getFiles('./pathHasNoMetadata')
+    assert "'metadata.json' does not exist in path './pathHasNoMetadata/01_folder'" in str(exc.value)
