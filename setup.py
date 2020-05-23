f07a7346bb66b23d42d6d852a093ddf969b38939
Joshua Davis <me@jdav.is>
2020-05-22 21:40:15 -0400

    working on setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -18,12 +18,20 @@ setup(
     author="Joshua Davis",
     author_email="me@jdav.is",
     url='https://github.com/Azure/Azure-Backlog-Generator',
-    version='0.1.1',
+    version='0.1.4',
     description='The Azure Backlog Generator (ABG) is designed to build backlogs for complex processes based on proven practices. The backlogs can be generated in either Azure DevOps or GitHub.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=['azbacklog'],
-    package_dir={'': 'src'},
+    packages=[
+        'src.azbacklog',
+        'src.azbacklog.entities',
+        'src.azbacklog.helpers',
+        'src.azbacklog.services',
+        'src.scripts',
+    ],
+    data_files=[
+        ('workitems/caf', ('workitems/caf/config.json'))
+    ],
     install_requires=[
         'pygithub'
     ],
--- src/scripts/azbacklog.py
+++ src/scripts/azbacklog.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-import azbacklog.helpers as helpers
-import azbacklog.services as services
+import src.azbacklog.helpers as helpers
+import src.azbacklog.services as services
 
 
 def run(args):
