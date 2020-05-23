bb5b4bdf37bd349324d104bf9b7a19084d9a6501
Joshua Davis <me@jdav.is>
2020-05-22 23:59:10 -0400

    adding workitem files to setup

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup
 from os import path
+import glob
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -13,12 +15,18 @@ with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as r:
         requirements.append(line.strip())
         line = r.readline()
 
+workitem_files = []
+directories = glob.glob('workitems/')
+for directory in directories:
+    files = glob.glob(directory+'*')
+    workitem_files.append((directory, files))
+
 setup(
     name='azbacklog',
     author="Joshua Davis",
     author_email="me@jdav.is",
     url='https://github.com/Azure/Azure-Backlog-Generator',
-    version='0.1.7',
+    version='0.1.8',
     description='The Azure Backlog Generator (ABG) is designed to build backlogs for complex processes based on proven practices. The backlogs can be generated in either Azure DevOps or GitHub.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -29,6 +37,7 @@ setup(
         'azbacklog.helpers',
         'azbacklog.services'
     ],
+    data_files=workitem_files,
     install_requires=[
         'pygithub'
     ],
