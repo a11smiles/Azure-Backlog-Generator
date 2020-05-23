a871612e9ffa4e82d824d859c831ecc0ee92fcfe
Joshua Davis <me@jdav.is>
2020-05-23 03:22:01 -0400

    updating version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     author="Joshua Davis",
     author_email="me@jdav.is",
     url='https://github.com/Azure/Azure-Backlog-Generator',
-    version='0.1.10',
+    version='0.1.12',
     description='The Azure Backlog Generator (ABG) is designed to build backlogs for complex processes based on proven practices. The backlogs can be generated in either Azure DevOps or GitHub.',
     long_description=long_description,
     long_description_content_type='text/markdown',
