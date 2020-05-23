60ff0ca066a44e3367d0e4a4bf9c5dcefdf2b60b
Joshua Davis <me@jdav.is>
2020-05-22 21:02:35 -0400

    correcting setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=['azbacklog'],
-    package_dir={'azbacklog': ['src', 'workitems']},
+    package_dir={'': 'src'},
     install_requires=[
         'pygithub'
     ],
